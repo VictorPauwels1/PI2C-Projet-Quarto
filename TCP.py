894bf59baa91f09f4f4a898054c625ec9f28a1b2
Pauwels Victor <23203@ecam.be>
2025-05-14 18:14:03 +0200

    Random fonctionnel

--- TCP.py
+++ TCP.py
@@ -1,12 +1,15 @@
 import socket
 import threading
 import json
+from random import randint
 
 class client (): 
     def __init__ (self) : 
+        matricule = input("Entrez votre matricule : ")
+        pseudo = input("Entrez votre pseudo : ")
         self.host = '0.0.0.0'
         #print (self.host)
-        self.portInscription = 2000
+        self.portInscription = int(input("Entrez le port d'inscription : "))
         s1 = socket.socket (type = socket.SOCK_STREAM)
         s1.bind ((self.host, self.portInscription))
         #s1.settimeout (0.5)
@@ -16,12 +19,12 @@ class client ():
         self.serveur = (self.IPserveur, self.portServeur)
         
         self.s1.connect (self.serveur)
-        self.portJeu = 3500
+        self.portJeu = int(input("Entrez le port de jeu : "))
         self.inscription = {
                                 "request": "subscribe",
                                 "port": self.portJeu,
-                                "name": "The Lion Ping",
-                                "matricules": ["23203"]
+                                "name": pseudo,
+                                "matricules": [matricule]
                             }
         self.reponse = json.dumps (self.inscription).encode()
         self.s1.sendall (self.reponse)
@@ -34,20 +37,59 @@ class client ():
         #self.s2.connect (self.serveur)
 
     def recept (self) : 
+        self.caseocc = []
+        self.piecesjouees = []
+        self.pieces = ["BDEC","BDEP","BDFC","BDFP","BLEC","BLEP","BLFC","BLFP","SDEC","SDEP","SDFC","SDFP","SLEC","SLEP","SLFC","SLFP"]
+        self.s2.listen()  
 
-        self.s2.listen()    
-        self.client, self.addr = self.s2.accept ()
-        data = self.client.recv(1024).decode()
+        while self.running == True : 
+            try :
+                  
+                self.client, self.addr = self.s2.accept ()
+                data = self.client.recv(1024)
 
-        retour = json.loads(data)
-        print (retour)
+                retour = json.loads(data.decode())
+                print (retour)
 
-        if retour["request"] == "ping" :
-            reponse = {"response" : "pong"}
-            envoi = json.dumps (reponse).encode()
-            self.client.sendall (envoi)
+                if retour["request"] == "ping" :
+                    reponse = {"response" : "pong"}
+                    envoi = json.dumps (reponse).encode()
+                    self.client.sendall (envoi)
+
+                if retour["request"] == "play" :
+                    if retour["state"]["piece"] == None : 
+                        pos = None
+                    else :
+                        while True : 
+                            pos = randint(0,15)  
+                            if retour["state"]["board"][pos] == None : 
+                                break
+                            else : 
+                                continue
+                        
+
+                    piece = randint (0,15)
+                    while self.pieces[piece] in retour["state"]["board"] or self.pieces[piece] == retour["state"]["piece"] :
+                        piece = randint (0,15)
+
+
+                    renvoi = {
+                        "response" : "move",
+                        "move" : {
+                            "pos" : pos,
+                            "piece" : self.pieces[piece]
+                        },
+                        "message" : ""
+                    }
+                    
+                    bidule = json.dumps (renvoi).encode()
+                    print (bidule)
+                    self.client.sendall (bidule)
+            except :
+                pass
 
     def run(self) : 
+        self.running = True
         threading.Thread(target=self.recept).start()
         
         try :    
